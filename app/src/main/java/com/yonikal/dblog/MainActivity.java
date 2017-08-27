6db5d514085a1ff7286fa7b06f20f6e63a4ca703
Yoni Kalangan <yoni.kalangan@gmail.com>
2017-08-27 08:13:45 +0300

    * Added DbLog as a dependency to the sample app * Added initialize with OnAfterCrash (not doing nothing with the file for now)

--- app/src/main/java/com/yonikal/dblog/MainActivity.java
+++ app/src/main/java/com/yonikal/dblog/MainActivity.java
@@ -3,11 +3,22 @@ package com.yonikal.dblog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.johnnyc.dblog.DBLog;
+import com.johnnyc.dblog.entities.OnAfterCrash;
+
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DBLog.initialize(this, new OnAfterCrash() {
+            @Override
+            public void doAfterCrash(File crashLogFile) {
+
+            }
+        });
     }
 }
