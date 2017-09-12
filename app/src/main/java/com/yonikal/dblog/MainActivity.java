cb6433bf13dc835cb9e1db638c873603447d858a
Yoni Kalangan <yoni.kalangan@gmail.com>
2017-09-12 10:05:45 +0300

    * Fixed sample code * Syncing between logs (Still not done)

--- app/src/main/java/com/yonikal/dblog/MainActivity.java
+++ app/src/main/java/com/yonikal/dblog/MainActivity.java
@@ -1,7 +1,9 @@
 package com.yonikal.dblog;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.johnnyc.dblog.DBLog;
@@ -10,12 +12,32 @@ import com.johnnyc.dblog.config.LogLevel;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int i = 1;
+    TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.logs);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         LogLevel[] logs = { LogLevel.ERROR, LogLevel.DEBUG };
         DBLog.initialize(this, logs, true, new OnAfterCrashStringBuilder() {
+
+            @Override
+            public void doAfterCrash(StringBuilder crashLogData) {
+                if(crashLogData.length() < 1) {
+                    tv.setText("Empty");
+                } else {
+                    tv.setText(crashLogData.toString());
+                }
+            }
+        }, new Handler());
+        /*DBLog.initialize(this, logs, true, new OnAfterCrashStringBuilder() {
 
             @Override
             public void doAfterCrash(StringBuilder crashLogData) {
@@ -23,9 +45,18 @@ public class MainActivity extends AppCompatActivity {
                 TextView tv = (TextView) findViewById(R.id.logs);
                 tv.setText(crashLogData.toString());
             }
+        });*/
+
+        findViewById(R.id.btn_add_log).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DBLog.d(this, String.valueOf(i));
+                i++;
+                DBLog.d(this, String.valueOf(i));
+                i++;
+                DBLog.d(this, String.valueOf(i));
+                i++;
+            }
         });
-        DBLog.d(this, "1");
-        DBLog.d(this, "2");
-        DBLog.d(this, "3");
     }
 }
